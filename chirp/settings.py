d73bb3e1bbe0112927ea62106fa1ce1ade1a4906
Martin Alexandru <alex.martin96@gmail.com>
2017-07-25 11:45:59 +0300

    ...

--- chirp/settings.py
+++ chirp/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 LOGIN_REDIRECT_URL = 'index'
